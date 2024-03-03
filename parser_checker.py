c500e4e52b4e8030834a07c72cd1c6d3766c2061
Keerthi Ramineni <131865513+aplha5@users.noreply.github.com>
2024-03-03 19:49:11 +0530

    Update parser_checker.py

--- parser_checker.py
+++ parser_checker.py
@@ -36,12 +36,58 @@ def p_statement(p):
                  | if_stmnt
                  | while
                  | function_call
-                 | compound_types'''
+                 | compound_types
+                 | compound_type_access'''
     # Handle individual statements here
     pass
 
 # Add other grammar rules here...
+def p_declaration(p):
+    '''declaration : 'var' type identifier '=' expression '''
+    pass
+def p_assignment(p):
+    '''assignment : identifier '=' expression'''
+    pass
+def p_type(p):
+    '''type : int
+            | bool
+            | str'''
 
+    pass
+def p_compound_types(p):
+    '''compound_types : A identifier P'''
+    pass
+def p_A(p):
+    '''A : tuple
+         | list'''
+    pass
+def p_K(p):
+    '''K : (data)
+         | [data]'''
+    pass
+def p_compounnd_type_access(p):
+    '''compounnd_type_access : Z F 
+                             | identifier expression'''
+    pass
+def p_Z(p):
+    '''Z : identifier '.' '''
+    pass
+def p_F(p):
+    '''F : add()
+         | front
+         |rear
+         |size()
+         |delete()'''
+    pass
+def p_if_stmnt(p):
+    '''if_stmnt :  'if' '(' condition ')' 'begin' statement_list 'end' T'''
+    pass
+def p_T(p):
+    '''T : 'elif' '(' condition ')' 'begin' statement_list 'end' K
+         | epsilon '''
+    pass
+
+    
 # Error rule for syntax errors
 def p_error(p):
     print("Syntax error in input:", p)
