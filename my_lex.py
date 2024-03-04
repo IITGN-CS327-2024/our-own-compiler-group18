d7d21773a8c442c9665934a4695c4edf774a32ce
Sudharshanbhardwaj <you@example.com>
2024-03-04 08:47:24 +0530

    first commit

---
+++ my_lex.py
@@ -0,0 +1,132 @@
+from ply import lex
+
+# Define tokens
+tokens = [
+    'EOF',
+    'ID',
+    'NUMBER',
+    'LPAREN',
+    'RPAREN',
+    'SEMI',
+    'COMMA',
+    'LSPAREN',
+    'RSPAREN',
+    'DOT',
+    'QUOTATION',
+    'ASSIGN',
+    'PLUS',
+    'PLUSPLUS',
+    'MINUS',
+    'MUL',
+    'DIV',
+    'REM',
+    'EQEQ',
+    'NOTEQ',
+    'GT',
+    'LT',
+    'GTEQ',
+    'LTEQ',
+    'AND',
+    'OR'
+]
+
+# Define keywords and reserved words
+keywords = {
+    'program': 'PROGRAM',
+    'var': 'VAR',
+    'int': 'INT',
+    'bool': 'BOOL',
+    'begin': 'BEGIN',
+    'end': 'END',
+    'if': 'IF',
+    'elif': 'ELIF',
+    'else': 'ELSE',
+    'while': 'WHILE',
+    'zout': 'ZOUT',
+    'str': 'STR',
+    'func': 'FUNC',
+    'return': 'RETURN',
+    'for': 'FOR',
+    'con': 'CON',
+    'tuple': 'TUPLE',
+    'list': 'LIST',
+    'add': 'ADD',
+    'size': 'SIZE',
+    'delete': 'DELETE',
+    'front': 'FRONT',
+    'rear': 'REAR',
+    'try': 'TRY',
+    'except': 'EXCEPT',
+    'true': 'TRUE',
+    'false': 'FALSE'
+}
+
+# Tokens with associated regular expressions
+t_ignore = ' \t'
+
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_SEMI = r';'
+t_COMMA = r','
+t_LSPAREN = r'\['
+t_RSPAREN = r'\]'
+t_DOT = r'\.'
+t_QUOTATION = r'\"'
+
+t_ASSIGN = r'='
+t_PLUS = r'\+'
+t_PLUSPLUS = r'\+\+'
+t_MINUS = r'-'
+t_MUL = r'\*'
+t_DIV = r'/'
+t_REM = r'%'
+t_EQEQ = r'=='
+t_NOTEQ = r'!='
+t_GT = r'>'
+t_LT = r'<'
+t_GTEQ = r'>='
+t_LTEQ = r'<='
+t_AND = r'&&'
+t_OR = r'\|\|'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = keywords.get(t.value.lower(), 'ID')
+    return t
+
+def t_COMMENT(t):
+    r'\/\/.*'
+    pass
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}' at line {t.lexer.lineno}")
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+# Example usage:
+if __name__ == "__main__":
+    text = """
+    program
+    var x = 10
+    for i in range(5):
+        zout "Hello, World!"
+    end
+    """
+    lexer.input(text)
+
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        print(tok)
