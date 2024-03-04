2e10088bb5d7fe73c91b3ad5c5172103daf5dcb1
Sudharshanbhardwaj <you@example.com>
2024-03-04 10:04:00 +0530

    latest commit

--- lexer_test.py
+++ lexer_test.py
@@ -1,19 +1,3 @@
-'''from lexer import *
-
-def main():
-
-    text = open("test_cases_lexer/test1.zeva","r").read()
-    # text = "2 + 3 "
-    
-    lexer = Lexer(text)
-    Token = lexer.get_token()
-    while Token.type != EOF:
-       
-        print(Token.__str__())
-        Token = lexer.get_token()
-    print(Token.value, Token.type)         # to check EOF
-
-main()'''
 from lexer import *
 
 def main():
